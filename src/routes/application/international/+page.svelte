e84f54e85c859cf87aab187d1660fa58e83b92d2
Alexander L <alex@alex8.com>
2023-10-22 06:10:02 +1100

    Update +page.svelte

--- src/routes/application/international/+page.svelte
+++ src/routes/application/international/+page.svelte
@@ -76,7 +76,14 @@
 										? 'bg-ttmfBeige'
 										: 'bg-white shadow-pricingShadow'}">
 									<div class="flex items-center gap-4">
-										<img src={el.icon} alt={el.title} />
+										{#if el.icon}
+											<img
+												src={el.icon}
+												alt="{el.title} Flag"
+												height="44"
+												width="44"
+												class="shadow rounded-xl" />
+										{/if}
 										<div class="text-left">
 											<p class="text-lg font-bold">{el.title}</p>
 											<p class="font-bold text-ttmfRed">AU${el.gov_fee + el.service_fee}</p>
