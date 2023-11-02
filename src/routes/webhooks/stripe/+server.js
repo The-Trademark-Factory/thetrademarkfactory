e7e22aab68432b31c10b95eca2da95378124e23a
TA SY TAN <sytanta@yahoo.com>
2023-11-02 11:54:59 +0700

    Use URLSearchParams() instead of FormData()

--- src/routes/webhooks/stripe/+server.js
+++ src/routes/webhooks/stripe/+server.js
@@ -44,7 +44,7 @@ export const POST = async ({ request }) => {
 
             const orderRefs = await firebaseDb
                 .collection('applications')
-                .where('paymentIntentId', '==', stripePaymentIntentId)
+                .where('paymentIntentId', '==', 'pi_3O7rJNGOBnDHsBHK0piC1mCT')
                 .get()
             if (!orderRefs?.size) throw error(400, 'Order not found 3: ' + stripePaymentIntentId + ' ' + orderRefs?.size)
 
@@ -70,41 +70,53 @@ export const POST = async ({ request }) => {
                 }
             } = orderData
 
-            if (import.meta.env.VITE_ENV_ADAPTER === 'netlify') {
-                fetch(`/.netlify/functions/sendToBasin?orderId=${order.id}&firstName=${firstName}&lastName=${lastName}&phone=${phone}&email=${email}&gst=${gst}&total=${total}`);
-            } else {
-                const formData = new FormData()
+            // if (import.meta.env.VITE_ENV_ADAPTER === 'netlify') {
+            //     fetch(`/.netlify/functions/sendToBasin?orderId=${order.id}&firstName=${firstName}&lastName=${lastName}&phone=${phone}&email=${email}&gst=${gst}&total=${total}`);
+            // } else {
+            //     const formData = new FormData()
 
-                formData.append('Name', `${firstName} ${lastName}`);
-                formData.append('Email', email);
-                formData.append('Phone', phone);
+            //     formData.append('Name', `${firstName} ${lastName}`);
+            //     formData.append('Email', email);
+            //     formData.append('Phone', phone);
 
-                formData.append(
-                    'GST',
-                    new Intl.NumberFormat(
+            //     formData.append(
+            //         'GST',
+            //         new Intl.NumberFormat(
+            //             'us-EN',
+            //             { style: 'currency', currency: 'AUD' }).format(gst)
+            //     );
+
+            //     formData.append(
+            //         'Price total',
+            //         new Intl.NumberFormat(
+            //             'us-EN',
+            //             { style: 'currency', currency: 'AUD' }).format(total)
+            //     );
+
+            //     formData.append('Order detail url', `${import.meta.env.VITE_PUBLIC_SITE_URL}/application/${order.id}`);
+
+            fetch(import.meta.env.VITE_USEBASIN_SUCCESS_FORM_URL, {
+                method: 'POST',
+                headers: { Accept: 'application/json' },
+                body: new URLSearchParams({
+                    firstName,
+                    lastName,
+                    email,
+                    phone,
+                    GST: new Intl.NumberFormat(
                         'us-EN',
-                        { style: 'currency', currency: 'AUD' }).format(gst)
-                );
-
-                formData.append(
-                    'Price total',
-                    new Intl.NumberFormat(
+                        { style: 'currency', currency: 'AUD' }).format(gst),
+                    'Price total': new Intl.NumberFormat(
                         'us-EN',
-                        { style: 'currency', currency: 'AUD' }).format(total)
-                );
-
-                formData.append('Order detail url', `${import.meta.env.VITE_PUBLIC_SITE_URL}/application/${order.id}`);
-
-                fetch(import.meta.env.VITE_USEBASIN_SUCCESS_FORM_URL, {
-                    method: 'POST',
-                    headers: { Accept: 'application/json' },
-                    body: formData
+                        { style: 'currency', currency: 'AUD' }).format(total),
+                    'Order detail url': `${import.meta.env.VITE_PUBLIC_SITE_URL}/application/${order.id}`
                 })
-                    .then(() => { })
-                    .catch((e) => {
-                        console.log('Error sending data to basin: ', e)
-                    });
-            }
+            })
+                .then(() => { })
+                .catch((e) => {
+                    console.log('Error sending data to basin: ', e)
+                });
+            // }
         }
     }
 
