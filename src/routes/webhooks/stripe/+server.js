1b8b88f31f59d7256e34662db1171149d6c3d3ad
TA SY TAN <sytanta@yahoo.com>
2023-11-02 09:18:59 +0700

    More webhook update

--- src/routes/webhooks/stripe/+server.js
+++ src/routes/webhooks/stripe/+server.js
@@ -46,62 +46,64 @@ export const POST = async ({ request }) => {
                 .collection('applications')
                 .where('paymentIntentId', '==', stripePaymentIntentId)
                 .get()
-            if (!orderRef?.length) throw error(400, 'Order not found 3: ' + stripePaymentIntentId + ' ' + orderRef?.length)
+            // if (!orderRef?.length) throw error(400, 'Order not found 3: ' + stripePaymentIntentId + ' ' + orderRef?.length)
 
-            const order = orderRef[0]
-            const orderData = order.data()
+            for (let order of orderRef) {
+                // const order = orderRef[0]
+                const orderData = order.data()
 
-            // Update the related firebase record
-            await firebaseDb.collection('applications').doc(order.id).update({
-                'stripe.status': "paid"
-            });
-
-            // Send data to basin 
-            const {
-                customerDetails: {
-                    firstName,
-                    lastName,
-                    phone,
-                    email
-                },
-                total: {
-                    gst,
-                    total
-                }
-            } = orderData
-
-            const formData = new FormData()
-
-            formData.append('Name', `${firstName} ${lastName}`);
-            formData.append('Email', email);
-            formData.append('Phone', phone);
-            formData.append('Stripe payment intent id', stripePaymentIntentId);
-
-            formData.append(
-                'GST',
-                new Intl.NumberFormat(
-                    'us-EN',
-                    { style: 'currency', currency: 'AUD' }).format(gst)
-            );
-
-            formData.append(
-                'Price total',
-                new Intl.NumberFormat(
-                    'us-EN',
-                    { style: 'currency', currency: 'AUD' }).format(total)
-            );
-
-            formData.append('Order detail url', `${import.meta.env.VITE_PUBLIC_SITE_URL}/application/${order.id}`);
-
-            fetch(import.meta.env.VITE_USEBASIN_SUCCESS_FORM_URL, {
-                method: 'POST',
-                headers: { Accept: 'application/json' },
-                body: formData
-            })
-                .then(() => { })
-                .catch(() => {
-                    console.log('Error sending data to basin: ', e)
+                // Update the related firebase record
+                await firebaseDb.collection('applications').doc(order.id).update({
+                    'stripe.status': "paid"
                 });
+
+                // Send data to basin 
+                const {
+                    customerDetails: {
+                        firstName,
+                        lastName,
+                        phone,
+                        email
+                    },
+                    total: {
+                        gst,
+                        total
+                    }
+                } = orderData
+
+                const formData = new FormData()
+
+                formData.append('Name', `${firstName} ${lastName}`);
+                formData.append('Email', email);
+                formData.append('Phone', phone);
+                formData.append('Stripe payment intent id', stripePaymentIntentId);
+
+                formData.append(
+                    'GST',
+                    new Intl.NumberFormat(
+                        'us-EN',
+                        { style: 'currency', currency: 'AUD' }).format(gst)
+                );
+
+                formData.append(
+                    'Price total',
+                    new Intl.NumberFormat(
+                        'us-EN',
+                        { style: 'currency', currency: 'AUD' }).format(total)
+                );
+
+                formData.append('Order detail url', `${import.meta.env.VITE_PUBLIC_SITE_URL}/application/${order.id}`);
+
+                fetch(import.meta.env.VITE_USEBASIN_SUCCESS_FORM_URL, {
+                    method: 'POST',
+                    headers: { Accept: 'application/json' },
+                    body: formData
+                })
+                    .then(() => { })
+                    .catch(() => {
+                        console.log('Error sending data to basin: ', e)
+                    });
+            }
         }
     }
 
