73beb048cba9c636d5410400ce474b1d6c5547cb
TA SY TAN <sytanta@yahoo.com>
2023-11-02 08:46:26 +0700

    More update for clearer Stripe error message

--- src/routes/webhooks/stripe/+server.js
+++ src/routes/webhooks/stripe/+server.js
@@ -23,94 +23,90 @@ export const POST = async ({ request }) => {
         throw error(400, { message: `Webhook Error: ${err.message}` })
     }
 
-    try {
-        // Handle the event
-        switch (event.type) {
-            case "checkout.session.completed": {
-                const orderRef = await firebaseDb.collection("applications").doc(event.data.object.id).get()
-                if (!orderRef.exists) throw error(404, { message: 'Order not found 1' })
+    // Handle the event
+    switch (event.type) {
+        case "checkout.session.completed": {
+            const orderRef = await firebaseDb.collection("applications").doc(event.data.object.id).get()
+            if (!orderRef.exists) throw error(404, 'Order not found 1')
 
-                const orderRef1 = await firebaseDb
-                    .collection("applications")
-                    .where('stripe.paymentIntentId', '==', event.data.object.payment_intent)
-                    .get()
+            const orderRef1 = await firebaseDb
+                .collection("applications")
+                .where('stripe.paymentIntentId', '==', event.data.object.payment_intent)
+                .get()
 
-                // Update the related firebase record
-                await firebaseDb.collection('applications').doc(event.data.object.id).update({
-                    'stripe.paymentIntentId': event.data.object.payment_intent
-                });
+            // Update the related firebase record
+            await firebaseDb.collection('applications').doc(event.data.object.id).update({
+                'stripe.paymentIntentId': event.data.object.payment_intent
+            });
 
-                break
-            }
-
-            case "payment_intent.succeeded": {
-                const stripePaymentIntentId = event.data.object.id
-                if (!stripePaymentIntentId) throw error(404, { message: 'Order not found 2' })
-
-                const orderRef = await firebaseDb
-                    .collection("applications")
-                    .where('stripe.paymentIntentId', '==', stripePaymentIntentId)
-                    .get()
-                if (!orderRef?.length) throw error(404, { message: 'Order not found 3' })
-
-                const order = orderRef[0]
-                const orderData = order.data()
-
-                // Update the related firebase record
-                await firebaseDb.collection('applications').doc(order.id).update({
-                    'stripe.status': "paid"
-                });
-
-                // Send data to basin 
-                const {
-                    customerDetails: {
-                        firstName,
-                        lastName,
-                        phone,
-                        email
-                    },
-                    total: {
-                        gst,
-                        total
-                    }
-                } = orderData
-
-                const formData = new FormData()
-
-                formData.append('Name', `${firstName} ${lastName}`);
-                formData.append('Email', email);
-                formData.append('Phone', phone);
-                formData.append('Stripe payment intent id', stripePaymentIntentId);
-
-                formData.append(
-                    'GST',
-                    new Intl.NumberFormat(
-                        'us-EN',
-                        { style: 'currency', currency: 'AUD' }).format(gst)
-                );
-
-                formData.append(
-                    'Price total',
-                    new Intl.NumberFormat(
-                        'us-EN',
-                        { style: 'currency', currency: 'AUD' }).format(total)
-                );
-
-                formData.append('Order detail url', `${import.meta.env.VITE_PUBLIC_SITE_URL}/application/${order.id}`);
-
-                fetch(import.meta.env.VITE_USEBASIN_SUCCESS_FORM_URL, {
-                    method: 'POST',
-                    headers: { Accept: 'application/json' },
-                    body: formData
-                })
-                    .then(() => { })
-                    .catch(() => {
-                        console.log('Error sending data to basin: ', e)
-                    });
-            }
+            break
         }
-    } catch (e) {
-        throw error(400, { message: e.message })
+
+        case "payment_intent.succeeded": {
+            const stripePaymentIntentId = event.data.object.id
+            if (!stripePaymentIntentId) throw error(404, 'Order not found 2')
+
+            const orderRef = await firebaseDb
+                .collection("applications")
+                .where('stripe.paymentIntentId', '==', stripePaymentIntentId)
+                .get()
+            if (!orderRef?.length) throw error(404, 'Order not found 3')
+
+            const order = orderRef[0]
+            const orderData = order.data()
+
+            // Update the related firebase record
+            await firebaseDb.collection('applications').doc(order.id).update({
+                'stripe.status': "paid"
+            });
+
+            // Send data to basin 
+            const {
+                customerDetails: {
+                    firstName,
+                    lastName,
+                    phone,
+                    email
+                },
+                total: {
+                    gst,
+                    total
+                }
+            } = orderData
+
+            const formData = new FormData()
+
+            formData.append('Name', `${firstName} ${lastName}`);
+            formData.append('Email', email);
+            formData.append('Phone', phone);
+            formData.append('Stripe payment intent id', stripePaymentIntentId);
+
+            formData.append(
+                'GST',
+                new Intl.NumberFormat(
+                    'us-EN',
+                    { style: 'currency', currency: 'AUD' }).format(gst)
+            );
+
+            formData.append(
+                'Price total',
+                new Intl.NumberFormat(
+                    'us-EN',
+                    { style: 'currency', currency: 'AUD' }).format(total)
+            );
+
+            formData.append('Order detail url', `${import.meta.env.VITE_PUBLIC_SITE_URL}/application/${order.id}`);
+
+            fetch(import.meta.env.VITE_USEBASIN_SUCCESS_FORM_URL, {
+                method: 'POST',
+                headers: { Accept: 'application/json' },
+                body: formData
+            })
+                .then(() => { })
+                .catch(() => {
+                    console.log('Error sending data to basin: ', e)
+                });
+        }
     }
 
     // Inform Stripe that the app has received the event
